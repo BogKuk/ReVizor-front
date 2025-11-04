1bfe78985d09cf8df064f3a0c79230025722fad2
Bogdan <bogdan-kuk2002@mail.ru>
2025-11-04 18:33:14 +0300

    added login request

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
-import axios from "axios";
-import {useEffect} from "react";
 import AuthorizationCard from "./components/AuthorizationCard.jsx";
 
 function App() {
-
-    const hello_world = () => {
-        axios.get('http://127.0.0.1:8000/helloworld').then((response) => {
-            console.log(response)
-        })
-    }
-
-    useEffect(() => {
-        hello_world()
-    }, []);
-
   return (
       <AuthorizationCard />
   )
--- src/components/AuthorizationCard.jsx
+++ src/components/AuthorizationCard.jsx
@@ -1,16 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import {KeyOutlined, UserOutlined} from '@ant-design/icons';
 import {Button, Card, Input, Space, Typography} from 'antd';
+import axios from "axios";
 
 const { Title } = Typography;
 
 const AuthorizationCard = () => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
     const [show, setShow] = useState(false);
 
     useEffect(() => {
         const timer = setTimeout(() => setShow(true), 50);
         return () => clearTimeout(timer);
     }, []);
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/authorization/login', {
+                login,
+                password
+            });
+            console.log('Ответ сервера:', response.data);
+            // здесь можно делать редирект или сохранять токен
+        } catch (error) {
+            console.error('Ошибка при логине:', error);
+            // здесь можно показать пользователю уведомление об ошибке
+        }
+    };
 
     return (
         <Card className={`fade-slide-up ${show ? 'show' : ''}`}
@@ -34,13 +51,19 @@ const AuthorizationCard = () => {
                     placeholder="Username"
                     prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
                     size="large"
+                    value={login}
+                    onChange={e => setLogin(e.target.value)}
                 />
                 <Input.Password
                     placeholder="Password"
                     prefix = {<KeyOutlined style={{ color: 'rgba(0,0,0,.25)' }}/>}
                     size="large"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                 />
-                <Button type="primary" size="large" block>Sign In</Button>
+                <Button type="primary" size="large" block onClick={handleLogin}>
+                    Sign In
+                </Button>
             </Space>
         </Card>
     );
